e6edf2e23f03bffbd9d33be3f29e0e3730e28d94
IonutGherman <ghermanionut96@gmail.com>
2022-12-15 15:36:57 +0200

    sweet: initialize vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+# Common device tree
+git clone https://github.com/ionutgherman/device_xiaomi_sm6150-common -b 13.0 device/xiaomi/sm6150-common
+# Kernel
+git clone https://github.com/itsshashanksp/kernel_xiaomi_sm6150 -b t13 kernel/xiaomi/sm6150
+# Vendor
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi -b 13.0 vendor/xiaomi
+# Xiaomi hardware
+git clone https://github.com/ionutgherman/hardware_xiaomi -b lineage-20 hardware/xiaomi
+# MIUI Camera
+git clone https://gitlab.com/mrfox2003/sweet-miuicamera -b thirteen vendor/xiaomi/sweet-miuicamera
+# Clang
+git clone https://gitlab.com/PixelOS-Devices/playgroundtc -b 16 prebuilts/clang/host/linux-x86/clang-playground
