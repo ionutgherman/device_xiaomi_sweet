de81ef5bbd9fedaf4e4e52cc6f68551405881338
IonutGherman <ghermanionut96@gmail.com>
2022-12-15 16:28:54 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/ionutgherman/device_xiaomi_sm6150-common -b 13.0 de
 # Kernel
 git clone https://github.com/itsshashanksp/kernel_xiaomi_sm6150 -b t13 kernel/xiaomi/sm6150
 # Vendor
-git clone https://github.com/PixelOS-Devices/vendor_xiaomi -b 13.0 vendor/xiaomi
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi -b thirteen vendor/xiaomi
 # Xiaomi hardware
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 # MIUI Camera
